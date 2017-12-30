9e72792ee17294bd352fd684ff68ab0f4bd855c2
Moku <mokuki082@gmail.com>
2017-12-30 21:59:00 +1100

    Added some commands

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 import sys
 import json
-import threading
-
-try:
-    from telegram.ext import Updater, CommandHandler
-    HAS_TELEGRAM = True
-except ImportError:
-    HAS_TELEGRAM = False
 
 
 class MokuBot():
     def __init__(self, token):
-        self.updater = Updater(config['token'])
-        self.updater.dispatcher.add_handler(CommandHandler('hello', self.hello))
+        # Initialize updater
+        self.updater = Updater(token)
+
+        # Add command handlers
+        hello_handler = CommandHandler('hello', self.hello)
+        self.updater.dispatcher.add_handler(hello_handler)
+        unknown_handler = MessageHandler(Filters.command, self.unknown)
+        self.updater.dispatcher.add_handler(unknown_handler)
+
+    def stop(self):
+        self.updater.stop()
 
     def hello(self, bot, update):
         text = 'Hello {}'.format(update.message.from_user.first_name)
+        update.message.reply_text(text)
+
+    def unknown(self, bot, update):
+        text = 'Sorry, I didn\'t understand that command.'
         update.message.reply_text(text)
 
     def run(self):
         self.updater.start_polling()
         self.updater.idle()
 
-if __name__ == '__main__':
-    if not HAS_TELEGRAM:
-        print('Error: Necessary package python-telegram-bot is missing')
-        print('To install: pip3 install python-telegram-bot')
-        exit(1)
 
+if __name__ == '__main__':
     # Load config file
     try:
         with open('config.json', 'r') as f:
@@ -44,7 +47,8 @@ if __name__ == '__main__':
         print('Error: Invalid config file.')
         exit(1)
 
+
+    # initialize objects
     mokubot = MokuBot(config['token'])
-    mokubot_t = threading.Thread(target=mokubot.run, daemon=True)
-    # Start the mokubot thread
-    mokubot_t.start()
+    # Start mokubot
+    mokubot.run()
